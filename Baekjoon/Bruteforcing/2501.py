e2504ec9c65d5a51a2588dd4a8ddc15fb1ebbac6
chorongfire33 <choyeon8317@naver.com>
2023-12-20 00:47:26 +0900

    Feat: 2051.py
    약수 구하기
    
    writer : 이민영

--- Baekjoon/Bruteforcing/2501.py
+++ Baekjoon/Bruteforcing/2501.py
@@ -1,3 +1,17 @@
 # 작성일자: 2023-12-19 TUE, 작성자 : AN밤비
 
 # 약수 구하기
+import sys
+
+numberN, ordinalNumbersK = map(int, sys.stdin.readline().split())
+
+divisorList = []
+
+for i in range(1, numberN + 1):
+    if numberN % i == 0:
+        divisorList.append(i)
+
+if divisorList and 1 <= ordinalNumbersK <= len(divisorList):
+    print(divisorList[ordinalNumbersK - 1])
+else:
+    print(0)
