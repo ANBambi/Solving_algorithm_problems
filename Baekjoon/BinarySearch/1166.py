70b7901a6b92fbea1205adca7cc39ca2a5057a98
chorongfire33 <choyeon8317@naver.com>
2023-12-23 01:41:10 +0900

    Fix: 1166.py
    동작하지 않은 1166 선물 문제 해결
    
    writer: 이민영

--- Baekjoon/BinarySearch/1166.py
+++ Baekjoon/BinarySearch/1166.py
@@ -2,23 +2,46 @@
 # 1166번 선물
 # 슈도코드
 
-함수 isPossible(중간, 길이, 갯수):
-    mid 길이로 만들 수 있는 선물의 수가 주어진 개수보다 크거나 같은지 확인
+# 함수 isPossible(중간, 길이, 갯수):
+#     mid 길이로 만들 수 있는 선물의 수가 주어진 개수보다 크거나 같은지 확인
 
-함수 binarySearch(길이, 박스갯수):
-    시작점, 끝점 = 1, max(length)
-    mid = (시작점 + 끝점) // 2
-    
+# 함수 binarySearch(길이, 박스갯수):
+#     시작점, 끝점 = 1, length
 
+#     결과 = 0
+#     while 시작점 <= 끝점:
+#         mid = (시작점 + 끝점) // 2
 
+#         if isPossible(중간, 길이, 갯수):
+#             결과 = mid
+#             시작점 = mid + 1
+#         else:
+#             끝점 = mid - 1
 
+#     결과 반환
 
-함수 main():
+# 함수 main():
+#     length, Count(박스갯수)
 
-    boxCount(박스갯수), length(세로), wide(가로), high(높이)
+#     결과 = binarySearch(길이, 박스갯수)
+#     결과 출력
 
-    결과 = binarySearch(길이, 박스갯수)
-    
+# main 함수 실행
 
-최댓값 A 출력
+n, l, w, h = map(int, input().split())
 
+start = 0
+end = max(l, w, h)
+result = 0
+
+for _ in range(100000):
+    mid = (start + end) / 2
+
+    cnt = (l // mid) * (w // mid) * (h // mid)
+
+    if cnt >= n:
+        result = mid
+        start = mid
+    else:
+        end = mid
+print("%0.10f" % result)
