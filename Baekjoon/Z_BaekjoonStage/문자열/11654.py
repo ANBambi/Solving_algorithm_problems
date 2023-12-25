c5d324c09c8df5a8c0506b31182afbab56e7bb68
chorongfire33 <choyeon8317@naver.com>
2023-12-25 23:30:26 +0900

    Feat: 11654.py
    ASCII CODE 개념정리
    
    writer: 이민영

---
+++ Baekjoon/Z_BaekjoonStage/문자열/11654.py
@@ -0,0 +1,17 @@
+# 작성일자: 2023-12-24 SUN, 작성자: AN밤비
+# 11654번 아스키 코드
+# 슈도코드
+
+# 입력받는 값 -> 알파벳을 아스키코드로
+# 출력값
+
+# ord : 입력받는 값은 알파벳, 출력하는 값은 알파벳에 할당된 아스키코드(숫자)
+
+asciiNumber = ord(input())
+print(asciiNumber)
+
+# 반대
+# chr : 입력받는 값은 아스키코드(숫자), 출력하는 값은 아스키코드에 해당하는 알파벳
+
+asciiCode = chr(int(input()))
+print(asciiCode)
