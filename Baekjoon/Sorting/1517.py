cdb66326d842322f4823398ec61eaac0b3d80610
chorongfire33 <choyeon8317@naver.com>
2024-03-23 00:10:50 +0900

    docs: 1517.py 17_들여쓰기 수정
    writer : ANBambi

--- Baekjoon/Sorting/1517.py
+++ Baekjoon/Sorting/1517.py
@@ -1,6 +1,7 @@
 import sys
 
 input = sys.stdin.readline
+
 result = 0
 
 
@@ -13,27 +14,27 @@ def mergeSort(start, end):
     mergeSort(middle + 1, end)
     for i in range(start, end + 1):
         tmp[i] = A[i]
-        k = start
-        index1 = start
-        index2 = middle + 1
-        while index1 <= middle and index2 <= end:
-            if tmp[index1] > tmp[index2]:
-                A[k] = tmp[index2]
-                result = result + index2 - k
-                k += 1
-                index2 += 1
-            else:
-                A[k] = tmp[index1]
-                k += 1
-                index1 += 1
-        while index1 <= middle:
+    k = start
+    index1 = start
+    index2 = middle + 1
+    while index1 <= middle and index2 <= end:
+        if tmp[index1] > tmp[index2]:
+            A[k] = tmp[index2]
+            result = result + index2 - k
+            k += 1
+            index2 += 1
+        else:
             A[k] = tmp[index1]
             k += 1
             index1 += 1
-        while index2 <= end:
-            A[k] = tmp[index2]
-            k += 1
-            index2 += 1
+    while index1 <= middle:
+        A[k] = tmp[index1]
+        k += 1
+        index1 += 1
+    while index2 <= end:
+        A[k] = tmp[index2]
+        k += 1
+        index2 += 1
 
 
 N = int(input())
